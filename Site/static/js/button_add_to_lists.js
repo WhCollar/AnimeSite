9bfbfe766998dfddbd0f38d433df8f66a2b90d17
WhCollar <monserka@gmail.com>
2022-02-09 22:10:49 +0500

    Добавил кнопочки для списков, ещё надо будет добавить флажок "Просмотрено" в верхнем левом уголочке постера, естественно выводиться он будет, если аниме находиться в списке "Просмотренно".

---
+++ Site/static/js/button_add_to_lists.js
@@ -0,0 +1,8 @@
+/* Для активации лайка */
+$(function(){
+  $('.like-toggle').click(function(){
+    $(this).toggleClass('like-active');
+    $(this).next().toggleClass('hidden-like-text');
+  });
+});
+/* END Для активации лайка */
--- Site/static/js/video-selector.js
+++ Site/static/js/video-selector.js
@@ -1,31 +1,29 @@
 /* Установка дефолтных значений в FormControlSelect2 и FormControlSelect3 */
-window.onload = function() {
-    let dubbingSelectValue = document.getElementById('FormControlSelect1').value;
-    let dubbingData = document.getElementById('dubbing_studio_for_title');
-    let playerForDubbingStudio = dubbingData.querySelector('[data-studio-id="'+dubbingSelectValue+'"]').children;
+let dubbingSelectValue = document.getElementById('FormControlSelect1').value;
+let dubbingData = document.getElementById('dubbing_studio_for_title');
+let playerForDubbingStudio = dubbingData.querySelector('[data-studio-id="'+dubbingSelectValue+'"]').children;
 
-    for (let node of playerForDubbingStudio){
-        let newOption = new Option(node.getAttribute('data-player-title'), node.getAttribute('data-player-id'));
-        document.getElementById('FormControlSelect2').add(newOption, undefined);
-    };
-    let playerSelectValue = document.getElementById('FormControlSelect2').value;
-    let iframes = document.getElementById('iframes_for_player').children;
+for (let node of playerForDubbingStudio){
+    let newOption = new Option(node.getAttribute('data-player-title'), node.getAttribute('data-player-id'));
+    document.getElementById('FormControlSelect2').add(newOption, undefined);
+};
+let playerSelectValue = document.getElementById('FormControlSelect2').value;
+let iframes = document.getElementById('iframes_for_player').children;
 
-    for (let node of iframes){
-        if (node.getAttribute('data-iframe-dubbing-id') == dubbingSelectValue) {
-            if (node.getAttribute('data-iframe-player-id') == playerSelectValue) {
-                let newOption = new Option(node.getAttribute('data-iframe-number-of-episodes'), node.getAttribute('data-iframe-id'));
-                document.getElementById('FormControlSelect3').add(newOption, undefined);
-            }
+for (let node of iframes){
+    if (node.getAttribute('data-iframe-dubbing-id') == dubbingSelectValue) {
+        if (node.getAttribute('data-iframe-player-id') == playerSelectValue) {
+            let newOption = new Option(node.getAttribute('data-iframe-number-of-episodes'), node.getAttribute('data-iframe-id'));
+            document.getElementById('FormControlSelect3').add(newOption, undefined);
         }
     }
+}
 
-    let episodesSelectIframeId = document.getElementById('FormControlSelect3').value;
-    for (let node of iframes){
-        if (node.getAttribute('data-iframe-id') == episodesSelectIframeId) {
-            var clonedNode = node.cloneNode(true);
-            document.getElementById('iframeBoxForVideo').appendChild(clonedNode);
-        }
+let episodesSelectIframeId = document.getElementById('FormControlSelect3').value;
+for (let node of iframes){
+    if (node.getAttribute('data-iframe-id') == episodesSelectIframeId) {
+        var clonedNode = node.cloneNode(true);
+        document.getElementById('iframeBoxForVideo').appendChild(clonedNode);
     }
 }
 
